ea53e98acd718659519f86e538c0fe2f31fb694c
ljyeonature <ljyeon131@gmail.com>
2023-03-20 18:36:33 +0900

    머스테치-게시글 조회,수정,삭제 화면 및 기능 추가

--- src/main/java/com/jojoldu/book/spingboot/service/posts/PostsService.java
+++ src/main/java/com/jojoldu/book/spingboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.jojoldu.book.spingboot.service.posts;
 
 import com.jojoldu.book.spingboot.domain.posts.Posts;
 import com.jojoldu.book.spingboot.domain.posts.PostsRepository;
+import com.jojoldu.book.spingboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.spingboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.spingboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.spingboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +40,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
